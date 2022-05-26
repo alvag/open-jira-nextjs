db797f60b24af835e52ee2c7fd14c210e6916715
Max Alva Garcia <maxrober.alvagarcia@everis.nttdata.com>
2022-05-26 16:44:52 -0500

    Manejo de estados para el sidebar

---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,53 @@
+import { Box, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import { useContext } from 'react';
+import { UIContext } from '../../context/ui';
+
+
+const menuItems: string[] = [ 'Inbox', 'Starred', 'Send email', 'Drafts' ];
+
+export const Sidebar = () => {
+
+    const { sideMenuOpen, closeSideMenu } = useContext( UIContext );
+
+    return (
+        <Drawer anchor={ 'left' }
+                open={ sideMenuOpen }
+                onClose={ closeSideMenu }>
+
+            <Box sx={ { width: 250 } }>
+                <Box sx={ { padding: '5px 10px' } }>
+                    <Typography variant={ 'h4' }>
+                        Menu
+                    </Typography>
+                </Box>
+
+                <List>
+                    { menuItems.map( ( item, index ) => (
+                        <ListItem key={ index }>
+                            <ListItemIcon>
+                                { index % 2 ? <InboxOutlinedIcon/> : <MailOutlineOutlinedIcon/> }
+                            </ListItemIcon>
+                            <ListItemText primary={ item }/>
+                        </ListItem>
+                    ) ) }
+                </List>
+
+                <Divider/>
+
+                <List>
+                    { menuItems.map( ( item, index ) => (
+                        <ListItem key={ index }>
+                            <ListItemIcon>
+                                { index % 2 ? <InboxOutlinedIcon/> : <MailOutlineOutlinedIcon/> }
+                            </ListItemIcon>
+                            <ListItemText primary={ item }/>
+                        </ListItem>
+                    ) ) }
+                </List>
+            </Box>
+
+        </Drawer>
+    );
+};
---
+++ context/ui/UIProvider.tsx
@@ -0,0 +1,27 @@
+import { FC, PropsWithChildren, useReducer } from 'react';
+import { UIContext } from './UIContext';
+import { uiReducer } from './uiReducer';
+
+export interface UIState {
+    sideMenuOpen: boolean;
+}
+
+const UI_INITIAL_STATE: UIState = {
+    sideMenuOpen: false
+};
+
+export const UIProvider: FC<PropsWithChildren<any>> = ( { children } ) => {
+    const [ state, dispatch ] = useReducer( uiReducer, UI_INITIAL_STATE );
+    const openSideMenu = () => dispatch( { type: 'UI - Open Sidebar' } );
+    const closeSideMenu = () => dispatch( { type: 'UI - Close Sidebar' } );
+
+    return (
+        <UIContext.Provider value={ {
+            ...state,
+            openSideMenu,
+            closeSideMenu
+        } }>
+            { children }
+        </UIContext.Provider>
+    );
+};
